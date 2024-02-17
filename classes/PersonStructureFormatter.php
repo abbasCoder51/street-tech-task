5ad8884ccdafb3b093756b43440d29793a63f6a1
3acdigital <3acdigital@gmail.com>
2024-02-17 14:55:56 +0000

    - Added DocBlocking for the PersonstructureFormatter class

--- classes/PersonStructureFormatter.php
+++ classes/PersonStructureFormatter.php
@@ -5,51 +5,52 @@ namespace Classes;
 class PersonStructureFormatter 
 {
     /**
-     * Title of person
+     * Title of person.
      * 
      * @var string
      */
     private string $title;
 
     /**
-     * First name of person
+     * First name of person.
      * 
      * @var string|null
      */
     private ?string $firstName;
 
     /**
-     * Initial of person
+     * Initial of person.
      * 
      * @var string|null
      */
     private ?string $initial;
 
     /**
-     * Last name of person
+     * Last name of person.
      * 
      * @var string
      */
     private string $lastName;
 
     /**
-     * Process the data for the person
+     * Process the data for the person.
      * 
-     * @param string $person The person data to process
-     * @return array Returns an array containing the processed data
+     * @param string $person The person data to process.
+     * @return array Returns an array containing the processed data.
      */
     public function process(string $person): array
     {
         $person = str_replace(",", "", $person);
         $personArray = explode(" ", $person);
 
-        // Check if 'and' or '&' exists in the array, value should be greater than 0
+        // Check if 'and' or '&' exists within the array.
         $andFoundInArray = array_search('and', $personArray) || array_search('&', $personArray);
         if($andFoundInArray) {
 
             $andIndexLocation = array_search('and', $personArray) ? array_search('and', $personArray) : array_search('&', $personArray);
 
-            // Will need to create a two dimensional array, because of the appearance of the 'and' or '&'
+            // Create a two dimensional array, to split names into two seperate arrays
+            // because of the appearance of the 'and' or '&'.
             $leftPart = array_splice($personArray, 0, $andIndexLocation);
             $rightPart = array_splice($personArray, 1);
 
@@ -76,16 +77,20 @@ class PersonStructureFormatter
     }
 
     /**
-     * Initialise the data for the person
+     * Initialise the data for the person.
      * 
-     * @param array $person The person data to initialise
+     * The length of each person array item is checked to ensure that the title, firstName, 
+     * initial and lastName properties are correctly set.
+     * 
+     * If the length is 2, set the title and lastName properties.
+     * if the length is 3, set the title, firstName/ initial 
+     * (Check length of string) and lastName properties.
+     * 
+     * @param array $person The person data to initialise.
      * @return void
      */
     public function initialise(array $person): void
     {
-        // Need to check length of array, 
-        // If = 2, then title and lastName
-        // If = 3, then either firstName or initial, have to check the name of 1st index file, a single character or a single character and a dot
         $this->title = $person[0];
         $this->firstName = count($person) == 2 ? null : ((strlen($person[1]) <= 2) ? null : $person[1]);
         $this->initial = strlen($person[1]) <= 2 ? $person[1] : null;
@@ -93,7 +98,7 @@ class PersonStructureFormatter
     }
 
     /**
-     * Display the data of the person
+     * Display the data of the person.
      * 
      * @return array returns an array of the person
      */
