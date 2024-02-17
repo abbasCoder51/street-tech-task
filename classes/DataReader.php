06549fda87c39c44c32c47cf5181d302bf32b3a1
3acdigital <3acdigital@gmail.com>
2024-02-17 15:02:05 +0000

    - Added doc blocking for the DataReader class

--- classes/DataReader.php
+++ classes/DataReader.php
@@ -8,18 +8,38 @@ use Interfaces\ProcessDataInterface;
 
 abstract class DataReader implements FetchDataInterface, ProcessDataInterface, PrintDataInterface
 {
+    /**
+     * Data from the reader.
+     * 
+     * @var mixed
+     */
     protected $data;
 
+    /**
+     * Fetch the data from the reader.
+     * 
+     * @return void
+     */
     public function fetchData($data): void
     {
         $this->data = $data;
     }
 
+    /**
+     * Process the data in the object.
+     * 
+     * @return void
+     */
     public function processData(): void
     {
         $this->data;
     }
 
+    /**
+     * Return the data in the object.
+     * 
+     * @return mixed
+     */
     public function printData()
     {
         return $this->data;
